b50f1c3bc7627b1fac37d6541799868c915e3bfe
Beth Videlock <107651322+videlock@users.noreply.github.com>
2024-09-04 12:41:32 -0700

    Update addNormalizationData.R

--- R/addNormalizationData.R
+++ R/addNormalizationData.R
@@ -11,22 +11,29 @@
 
 
 
+add.norm<-function(raw.dat,norm.dat.dir,norm.meth,batch.col){
+  raw.dat <- as.data.frame(raw.dat)
+  raw.dat[,batch.col] <- as.factor(raw.dat[,batch.col])
+  normfilelist<-list.files(norm.dat.dir)
+  dat_norm<-data.frame()
+  
+  for(normdat in normfilelist){
 
-for(normdat in normfiles){
+    ndat<-read96wells(mat = read.table(
+      file.path(norm.dat.dir,normdat),na.strings = na))
 
-  ndat<-read96wells(mat = read.table(
-    file.path(norm.dat.dir,normdat),na.strings = na))
+    rdat<-raw.dat[raw.dat[,batch.col]==normdat,]
 
-  rdat<-raw.dat[raw.dat[,batch.col]==normdat,]
+    for(i in 1:nrow(rdat)){
 
-  for(i in 1:nrow(rdat)){
-
-    y=ndat$Value[ndat$well==rdat$well[i]]
-    if(!is.na(y)&rdat$wave.id[i]!="Background"){
-      rdat[i,norm.meth]<-y
-    }else{rdat[i,norm.meth]<-NA}
+      y=ndat$Value[ndat$well==rdat$well[i]]
+      if(!is.na(y)&rdat$wave.id[i]!="Background"){
+        rdat[i,norm.meth]<-y
+      }else{rdat[i,norm.meth]<-NA}
     
-  dat_norm<-rbind(dat_norm,rdat)
+    dat_norm<-rbind(dat_norm,rdat)
+  }
+  return(dat_norm)
+  }
 }
-return(dat_norm)
-}
+  
